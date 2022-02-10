eb75442e2a496a2f8b097a70c1a697c936215fcf
Jane Nogueira <jane.tecnicavocal@gmail.com>
2022-02-10 00:10:51 -0300

    Estudos sobre JavaScript

---
+++ aula_javascript/JS/main.js
@@ -0,0 +1,104 @@
+function botao()
+{
+    document.getElementById("agradecimento").innerHTML = "Obrigado (a) por clicar";
+    //console.log(document.getElementById("agradecimento"));
+    //alert("Obrigado(a) por clicar")
+}
+
+
+// var validar = 0; // variável global, fora da função
+// function validaIdade(idade)
+// {
+//     validar;
+//     if (idade >= 18)
+//     {
+//         validar = true
+//     }
+//     else
+//     {
+//         validar = false
+//     }
+//     return validar;
+// }
+
+// var idade = prompt ("Qual a sua idade?");
+// validaIdade(idade)
+// console.log(validar);
+
+// function soma(n1, n2)
+// {
+//     return n1 + n2;
+// }
+// alert (soma(4, 28));
+
+// function setReplace (frase, nome, novo_nome)
+// {
+//     return frase.replace(nome, novo_nome)
+// }
+// alert (setReplace("Vai Japão", "Japão", "Brasil"));
+
+
+// var d = new Date();
+//alert(d);
+//alert(d.getMonth()+1); //acrecentar o +1 para trazer o mês atual. 
+//alert(d.getMinutes());
+//alert(d.getDate());
+//  alert(d.getHours());
+
+
+
+
+// var count;
+// for(count = 0; count <=5; count++)
+// {
+//     alert(count);
+// }
+
+// var count = 0;
+// while (count <= 5)
+// {
+//     console.log(count);
+//     count++;
+//     alert(count);
+// }
+
+// var idade = prompt("Qual é a sua idade");
+// if (idade >= 18)
+// {
+//     alert("maior de idade");
+// }
+// else
+// {
+//     alert("menor de idade");
+// }
+
+// var frutas = [{nome:"maçã", cor:"vermelha"}, {nome: "uva", cor:"roxa"}]
+// console.log(frutas.nome);
+// alert(frutas[1].nome);
+
+// var fruta = {nome:"maçã", cor:"vermelha"}
+// console.log(fruta.nome);
+// alert(fruta.cor);
+// var lista = ["maçã ", "pêra ", "laranja "];
+// lista.push("uva ", "limão ");
+//lista.pop();
+// console.log(lista.length);
+// alert(lista);
+//console.log(lista.reverse()); // imprime o que estiver escrito de trás para frente
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+// var nome = "Jane Nogueira ";
+//var idade = 36;
+//var idade2 = 10;
+// var n1 = 20;
+// var n2 = 30;
+// var frase = "Japão é o melhor time do mundo";
+//alert(idade + idade2);
+//alert ("Bem vindo " + nome + idade + " anos");
+// console.log(n1 + n2);
+// console.log (nome);
+//console.log (idade + idade2);
+//console.log (frase.replace("Japão", "Brasil"));
+// alert(frase.replace("Japão", " Brasil"));
+// console.log(frase.toUpperCase());
